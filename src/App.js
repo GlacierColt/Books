5dc6741c24e62d6f41a56937c4fd64cee5ef5537
GlacierColt <99321198+GlacierColt@users.noreply.github.com>
2023-07-13 08:44:09 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+import axios from 'axios';
+
+
+function App()
+{
+    const [books,setBooks]=useState([]);
+
+    const fetchBooks = async () =>
+    {
+        const response = await axios.get('http://localhost:3001/books');
+        setBooks(response.data);
+    }
+
+    useEffect(() => {
+        fetchBooks();
+
+    }, []);
+
+    const deleteBook = async (id) => 
+    {
+        await axios.delete('http://localhost:3001/books/' + id)
+        const updatedBooks = books.filter((book) => 
+        {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const editBook = async (id, newTitle) =>
+    {
+        const response = await axios.put('http://localhost:3001/books/' + id, {title: newTitle});
+
+        const updatedBooks = books.map((book) => {
+            if (book.id === id)
+            {
+                return {...book, ...response.data};
+            }
+
+            return book;
+        });
+        setBooks(updatedBooks);
+
+    };
+
+    const createBook = async (title) =>
+    {
+        //console.log('Add book: ', title);
+        //const updatedBooks =[...books, {id: Math.round(Math.random()*100000), title}];
+        //setBooks(updatedBooks);
+
+       const response = await axios.post('http://localhost:3001/books', {title});
+       const updatedBooks = [...books, response.data];
+       setBooks(updatedBooks);
+    };
+
+    return <div className="app">
+        <h1>Reading List</h1>
+        <BookList onEdit={editBook} books={books} onDelete={deleteBook} />
+        <BookCreate onCreate={createBook} />
+    </div>;
+}
+
+export default App;
